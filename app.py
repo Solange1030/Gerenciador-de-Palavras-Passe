9cfac138bc968a04d8cc56d0ea35cf7bd0e279eb
Fernando Maleiane <maleianefernando3@gmail.com>
2025-10-01 13:15:12 +0200

    fix: fetch environment variables

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
+db_user = os.getenv("DB_USER")
+db_host = os.getenv("DB_HOST")
+db_password = os.getenv("DB_PASSWORD")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 db = SQLAlchemy()
 
@@ -10,7 +17,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/stegano_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_user}{':' + db_password if db_password else ''}@{db_host}/{db_name}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
