5b7fb5050f9f53957c92ab324f48eb556cac8d58
Fernando Maleiane <maleianefernando3@gmail.com>
2025-10-04 21:18:17 +0200

    fix: move flask instance to out of create_app function

--- app.py
+++ app.py
@@ -12,10 +12,9 @@ db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
 db = SQLAlchemy()
-
+app = Flask(__name__)
 
 def create_app():
-    app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_user}{':' + db_password if db_password else ''}@{db_host}/{db_name}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
