5ac095e0ee9d3de988bc400f3c7a3a696a852429
Solange1030 <solangemarialurdes@gmail.com>
2025-10-21 20:09:52 +0200

    token added

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,8 +1,9 @@
 from extensions import db
-from flask import jsonify, redirect, url_for
+from flask import jsonify, make_response, redirect, url_for
 from models import User
 from utils import utils, crypto
 from email_validator import validate_email, EmailNotValidError
+from utils import token_utils
 
 
 def sign_up (data):
@@ -44,7 +45,7 @@ def sign_in(data):
     if user:
         user.otp_code = utils.gen_otp()
         db.session.commit()
-        return jsonify({"message": "OTP gerado, ", "otp": user.password_master})
+        return jsonify({"message": "OTP gerado, ", "otp": user.otp_code, "redirect_to": "/auth/check_otp"})
     else:
         return jsonify({"message": "Email nao achado "})   
 
@@ -54,7 +55,11 @@ def otp_varificate(otp_data):
     user = User.query.filter_by( password_master = otp_required).first()
     if user:
         user.otp_code = ""
-        return jsonify({"message": "Bem vinda " + user.name})
+        token = token_utils.generate_token(user)
+        resp = make_response(redirect(url_for("passwords")))
+        resp.set_cookie('token', token, httponly=True, samesite='Strict', max_age=7200)
+        
+        return resp
     else:
         return jsonify({"message": "Invalido OTP"}), 401
 
---
+++ utils/token_utils.py
@@ -0,0 +1,60 @@
+import jwt
+import datetime
+import os
+from flask import request, jsonify
+from functools import wraps
+from models.User import User
+from extensions import db
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def generate_token(user):
+    
+    payload = {
+        'user_id': user.id,
+        'email': user.email,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+
+
+def verify_token(token):
+    
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        return data
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
+
+
+def token_required(f):
+    
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        
+        if 'Authorization' in request.headers:
+            try:
+                token = request.headers['Authorization'].split(" ")[1]
+            except IndexError:
+                pass
+        elif 'token' in request.cookies:
+            token = request.cookies.get('token')
+
+        if not token:
+            return jsonify({'message': 'Token ausente'}), 401
+
+        data = verify_token(token)
+        if not data:
+            return jsonify({'message': 'Token inválido ou expirado'}), 401
+
+        current_user = User.query.get(data['user_id'])
+        if not current_user:
+            return jsonify({'message': 'Usuário não encontrado'}), 404
+
+        return f(current_user, *args, **kwargs)
+    return decorated
